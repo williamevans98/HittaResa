ff0cf7737d9cec1ebc65874895194b54f59c6946
Dasa-ia-student <daniel_sandgren@yahoo.se>
2021-03-24 21:44:02 +0100

    filter och pythonkod
    Testar att lägga till en filtersida

--- HTML/test.py
+++ HTML/test.py
@@ -10,6 +10,10 @@ def home():
 def gillar():
     return render_template("gillar.html")
 
+@app.route("/filter")
+def filter():
+    return render_template("filter.html")
+
 if __name__ == '__main__':
     app.defug = True
     app.run(host = '127.0.0.1', port = 5000)
