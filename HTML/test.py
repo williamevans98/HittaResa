cdf46afb550cea4e7d7cf7e7a0f2d365f5842c0e
Flloyden <lloyd.adam99@gmail.com>
2021-03-24 16:57:56 +0100

    Testat att lägga till cards

--- HTML/test.py
+++ HTML/test.py
@@ -1,10 +1,14 @@
 from flask import Flask, redirect, url_for, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
 def home():
     return render_template("Hitta_resa.html")
+
+@app.route("/gillar")
+def gillar():
+    return render_template("gillar.html")
 
 if __name__ == '__main__':
     app.defug = True
