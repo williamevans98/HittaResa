b891582d5cf1846617e4b0144ed7b19acb31043d
Flloyden <lloyd.adam99@gmail.com>
2021-03-25 11:50:27 +0100

    Gjort en navigation
    En navigation med fungerande route. Innehåller en logga. Funkar till desktop inte fullt responsiv.
    
    Co-Authored-By: Dasa-ia-student <81235028+Dasa-ia-student@users.noreply.github.com>

--- HTML/test.py
+++ HTML/test.py
@@ -4,11 +4,19 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route("/")
 def home():
-    return render_template("Hitta_resa.html")
+    return render_template("index.html")
 
 @app.route("/gillar")
 def gillar():
     return render_template("gillar.html")
+
+@app.route("/inloggning")
+def inloggning():
+    return render_template("logga-in.html")
+
+@app.route("/om")
+def om():
+    return render_template("om.html")
 
 @app.route("/filter")
 def filter():
