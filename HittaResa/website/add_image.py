8a086cb224ea3741e36170157530c3f800a96a71
Flloyden <lloyd.adam99@gmail.com>
2021-04-15 15:02:18 +0200

    Lagt till kommentarer

--- HittaResa/website/add_image.py
+++ HittaResa/website/add_image.py
@@ -1,17 +1,18 @@
 import pymysql
 
-# Open database connection
+# Öppnar anslutningen till databasen.
 connection = pymysql.connect(host="sql11.freemysqlhosting.net", user="sql11405569", passwd="8M3yX3fj8V", database="sql11405569")
-# prepare a cursor object using cursor() method
+
+# Förbereder ett "cursor" objekt genom att använda cursor() metoden.
 cursor = connection.cursor()
 
 # Använd denna när man lägger till nya bilder.
-# Ange hela url och ange rätt namn på resmålet
+# Ange hela url och ange rätt namn på resmålet.
 insert1 = "INSERT INTO images (location, url) values('Venedig', 'static/images/destinationsbilder-test/1.jpg');"
 
-#executing the quires
+# Exekverar frågan som görs till databasen.
 cursor.execute(insert1)
 
-#commiting the connection then closing it.
+# Commitar anslutningen för att sedan stänga den.
 connection.commit()
 connection.close()
