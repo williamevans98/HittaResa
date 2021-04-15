a33c89abc97f86ff5d5d04e52dc133711a362b28
Flloyden <lloyd.adam99@gmail.com>
2021-04-15 15:12:26 +0200

    Update views.py

--- HittaResa/website/views.py
+++ HittaResa/website/views.py
@@ -4,9 +4,10 @@ from flask_login import login_required, current_user
 from . import db
 import pymysql
 
-# Open database connection
+# Öppnar anslutningen till databasen.
 connection = pymysql.connect(host="sql11.freemysqlhosting.net", user="sql11405569", passwd="8M3yX3fj8V", database="sql11405569")
-# prepare a cursor object using cursor() method
+
+# Förbereder ett "cursor" objekt genom att använda cursor() metoden.
 cursor = connection.cursor()
 
 # Definierar views som blueprints för att vi ska kunna använda det som templates
