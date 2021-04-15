617b34a701930dd87fe6133e8f98af938222a80d
Flloyden <lloyd.adam99@gmail.com>
2021-04-15 15:07:09 +0200

    Update auth.py

--- Använd denna/website/auth.py
+++ Använd denna/website/auth.py
@@ -3,9 +3,25 @@ from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
+<<<<<<< Updated upstream:Använd denna/website/auth.py
 auth = Blueprint('auth', __name__)
 
 @auth.route('/inloggning', methods=['GET', 'POST'])
+=======
+import pymysql
+
+# Öppnar anslutningen till databasen.
+connection = pymysql.connect(host="sql11.freemysqlhosting.net", user="sql11405569", passwd="8M3yX3fj8V", database="sql11405569")
+
+# Förbereder ett "cursor" objekt genom att använda cursor() metoden.
+cursor = connection.cursor()
+
+# Länkar auth.py med main.py
+auth = Blueprint('auth', __name__)
+
+# Hämtar alla fält och försöker logga in användaren
+@auth.route('/inloggning', methods=['POST'])
+>>>>>>> Stashed changes:HittaResa/website/auth.py
 def login():
     if request.method == 'POST':
         email = request.form.get('email')
@@ -23,13 +39,22 @@ def login():
 
     return render_template("inloggning.html", user=current_user)
 
+<<<<<<< Updated upstream:Använd denna/website/auth.py
+=======
+# Loggar ut användaren
+>>>>>>> Stashed changes:HittaResa/website/auth.py
 @auth.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('auth.login'))
 
+<<<<<<< Updated upstream:Använd denna/website/auth.py
 @auth.route('/registrera.html', methods=['GET', 'POST'])
+=======
+# Hämtar alla fält och registrerar användaren
+@auth.route('/registrera', methods=['POST'])
+>>>>>>> Stashed changes:HittaResa/website/auth.py
 def sign_up():
     if request.method == 'POST':
         email = request.form.get('email')
@@ -59,6 +84,7 @@ def sign_up():
 
     return render_template("registrera.html", user=current_user)
 
+<<<<<<< Updated upstream:Använd denna/website/auth.py
 
 #En route till gillar-sidan
 @auth.route('/gillar')
@@ -66,16 +92,35 @@ def gillar():
     return render_template("gillar.html")
 
 #En route till inloggningssidan
+=======
+# En route till gillar-sidan
+@auth.route('/gillar')
+def gillar():
+    sql = ("SELECT * from images")
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    return render_template("gillar.html", user=current_user, content=data)
+
+# En route till inloggningssidan
+>>>>>>> Stashed changes:HittaResa/website/auth.py
 @auth.route('/inloggning')
 def inloggning():
     return render_template("inloggning.html")
 
+<<<<<<< Updated upstream:Använd denna/website/auth.py
 #En route till registrera-sidan
+=======
+# En route till registrera-sidan
+>>>>>>> Stashed changes:HittaResa/website/auth.py
 @auth.route('/registrera')
 def registrera():
     return render_template("registrera.html")
 
+<<<<<<< Updated upstream:Använd denna/website/auth.py
 #En route till om-sidan
+=======
+# En route till om-sidan
+>>>>>>> Stashed changes:HittaResa/website/auth.py
 @auth.route('/om')
 def om():
     return render_template("om.html")
