db7c456b22443840ad676457fc29b9b22a29eb54
RasmusAppelgren <75739932+RasmusAppelgren@users.noreply.github.com>
2021-04-25 10:01:50 +0200

    Update auth.py

--- HittaResa/website/auth.py
+++ HittaResa/website/auth.py
@@ -26,13 +26,13 @@ def login():
         user = User.query.filter_by(email=email).first()
         if user:
             if check_password_hash(user.password, password):
-                flash('Logged in successfully!', category='success')
+                flash('Inloggningen lyckades!', category='success')
                 login_user(user, remember=False, fresh=True)
                 return redirect(url_for('views.home'))
             else:
-                flash('Incorrect password, try again.', category='error')
+                flash('Felaktigt lösenord, försök igen', category='error')
         else:
-            flash('Email does not exist.', category='error')
+            flash('Email finns inte', category='error')
 
     return render_template("inloggning.html", user=current_user)
 
@@ -58,22 +58,22 @@ def sign_up():
 
         user = User.query.filter_by(email=email).first()
         if user:
-            flash('Email already exists.', category='error')
+            flash('Email finns redan', category='error')
         elif len(email) < 4:
-            flash('Email must be greater than 3 characters.', category='error')
+            flash('Email måste bestå av mer än 3 bokstäver', category='error')
         elif len(first_name) < 2:
-            flash('First name must be greater than 1 character.', category='error')
+            flash('Förnamnet måste vara längre än 1 bokstav', category='error')
         elif password1 != password2:
-            flash('Passwords does not match.', category='error')
+            flash('Lösenord matchar inte', category='error')
         elif len(password1) < 8:
-            flash('Password must be at least 8 characters.', category='error')
+            flash('Lösenordet måste vara mer än 8 karaktärer', category='error')
         else:
             new_user = User(email=email, first_name=first_name, password=generate_password_hash(
                 password1, method='sha256'))
             db.session.add(new_user)
             db.session.commit()
             #login_user(new_user, remember=False)
-            flash('Account created!', category='success')
+            flash('Konto skapat!', category='success')
             return redirect(url_for('views.home'))
 
     return render_template("registrera.html")
