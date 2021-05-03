fbeeabd49be317f69dd7db4c7d58f69df100c3a5
9xtryhx <70478007+9xtryhx@users.noreply.github.com>
2021-05-03 14:02:18 +0200

    removed login_manager and replaced it with user

--- HittaResa/website/models.py
+++ HittaResa/website/models.py
@@ -6,7 +6,7 @@ from sqlalchemy.sql import func
 
 
 class User(db.Model, UserMixin):
-    __tablename__ = 'login_manager'
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
