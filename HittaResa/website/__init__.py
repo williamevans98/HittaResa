32c8078dfba7a9a106abaf531c9d7b52418c6934
Flloyden <lloyd.adam99@gmail.com>
2021-05-03 16:34:42 +0200

    Problem med git

--- HittaResa/website/__init__.py
+++ HittaResa/website/__init__.py
@@ -1,21 +1,26 @@
+# Import av funktioner som vi använder
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+import wikipedia
+from cachetools import cached
 
+
+# Detta ska ändras
 db = SQLAlchemy()
-DB_NAME = "userdatabase.db"
+DB_NAME = "login_manager"
 
-# Import av funktioner som vi använder
+# Scriptet som fixar databasen
+
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'admin'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://sql11405569:8M3yX3fj8V@sql11.freemysqlhosting.net/sql11405569'
     db.init_app(app)
 
-# Konfigurerar SQLAlchemy
-
+    # Importerar allt från template, static o.s.v.
     from .views import views
     from .auth import auth
 
@@ -35,7 +40,22 @@ def create_app():
     def load_user(id):
         return User.query.get(int(id))
 
+    app.jinja_env.globals.update(fetch_from_wikipedia=fetch_from_wikipedia)
+
     return app
+
+
+
+# Hämta från wikipedia
+# Cache:ad för att få ner laddningstiden efter första gången
+# TTL (time to live) är by default satt till 10800s (3hrs), sen hämtas summaryn igen från wikipedia
+@cached(cache={})
+def fetch_from_wikipedia(title):
+    wikipedia.set_lang("sv")
+    summary = wikipedia.summary(title, sentences=2)
+    return summary
+
+# Om databasen inte existerar så skapas den
 
 
 def create_database(app):
