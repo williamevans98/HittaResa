6c1f91683e9efaf33ffc4a02366391e755e0bc73
williamevans98 <98william.evans@gmail.com>
2021-05-03 16:07:50 +0200

    Update add_image.py

--- HittaResa/website/add_image.py
+++ HittaResa/website/add_image.py
@@ -10,9 +10,31 @@ cursor = connection.cursor()
 # Ange hela url och ange rätt namn på resmålet.
 insert1 = "INSERT INTO images (location, url) values('Venedig', 'static/images/destinationsbilder-test/1.jpg');"
 
+
+#insert1 = "INSERT INTO status (user_id, image_id, like_or_not) values(26, 2, 1);"
+
 # Exekverar frågan som görs till databasen.
 cursor.execute(insert1)
 
 # Commitar anslutningen för att sedan stänga den.
 connection.commit()
 connection.close()
+
+
+
+
+
+#Använd på gillar.html
+SELECT * FROM `images`
+JOIN status on images.image_id = status.image_id
+WHERE like_or_not = 1 AND user_id = 26 
+
+
+#Använd denna för att visa upp alla bilder som inte har blivit gillade för en inloggad användare
+#Denna måste dock kopplas till rätt användare för att visa upp rätt bilder till rätt användare (26 kommer alltså ersättas med användarens ID )
+SELECT * FROM `images` WHERE NOT EXISTS (SELECT * FROM status WHERE status.image_id = images.image_id AND status.user_id = 26)
+
+
+
+#Använd på index för att visa upp alla bilder för en användare som inte är inloggad 
+SELECT * FROM `images` WHERE NOT EXISTS (SELECT * FROM status WHERE status.image_id = images.image_id AND status.user_id = NULL)
