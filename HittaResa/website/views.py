775ca67bb76a334d7bcee118d8f1d11d85d042eb
Flloyden <lloyd.adam99@gmail.com>
2021-05-26 15:33:24 +0200

    Småfix

--- HittaResa/website/views.py
+++ HittaResa/website/views.py
@@ -15,7 +15,7 @@ def home():
     Hämtar bild-länken från databasen och returnerar den som "data".
     '''
     # Skapar en connection till databasen.
-    connection = pymysql.connect(host="sql11.freemysqlhosting.net",user="sql11413883",passwd="2t3rFh95M7",database="sql11413883")
+    connection = pymysql.connect(host=database_host,user=database_user,passwd=database_password,database=database_name)
     # Förbereder ett "cursor" objekt genom att använda cursor() metoden.
     cursor = connection.cursor()
     string = str(current_user)
